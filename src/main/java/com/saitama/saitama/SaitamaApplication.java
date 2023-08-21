20239fc9b4c6a1e1b950967a4b4d9f501234b0ea
JiangTaoHou <925147954@qq.com>
2023-08-21 09:48:30 +0800

    初始化项目

---
+++ src/main/java/com/saitama/saitama/SaitamaApplication.java
@@ -0,0 +1,13 @@
+package com.saitama.saitama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaitamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaitamaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saitama/saitama/SaitamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saitama.saitama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaitamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
