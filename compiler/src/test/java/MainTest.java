852e7828b93e4cab63436b4be8d9b3e507c7fb0d
houjiangtao <925147954@qq.com>
2024-11-27 16:09:13 +0800

    1.增加测试

---
+++ compiler/src/test/java/MainTest.java
@@ -0,0 +1,20 @@
+public class MainTest {
+    //TODO 构造器调用得调用super.
+    //TODO 编译单元就是包含类的声明，然后结束，应当包含一个类声明的对象
+    //TODO 类声明应该是一个类的名字然后ClassName "{"  方法体  "}"
+    //TODO 方法体应该是多个字段和多个方法
+    //TODO 字段声明是 类型，变量名
+    //TODO 方法声明是 类型可选，void就可以不填，方法名 ，然后 (参数列表) 然后方法体
+    //TODO 参数列表有可能是Java的参数声明方式，也可以是参数有默认值的，C++和Kotlin
+    //TODO 类型分为原始类型和对象类型
+    //TODO 方法体就是block,就是大括号，大括号里面就是语句，都有什么语句？
+    // 变量声明，重新复制，打印语句，for循环语句，return语句，if语句，和表达式，还有block本身
+    // 比如if(){}这个statement就是一个block，而且if可以有括号，也可以没有
+    //TODO 返回语句如果返回了一个表达式，那么就是正常的返回语句，如果没有就是void的
+    //
+    //TODO 调用方法传参，可以默认的参数位置，和Java一样，也可以指定参数的名字。
+    // sayHai("will",19),sayHai(name->"will",age->19)
+
+    //TODO StatementGenerator属性有ExpressionGenerator整体，还有其他的Statement
+    //TODO ExpressionGenerator 有许多的ExpressionGenerator
+}
